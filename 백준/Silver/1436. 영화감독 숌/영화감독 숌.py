4cffde50e133fc5f9a3a5ec53ce757abf626310d
YeeImHandsome <153816302+YeeImHandsome@users.noreply.github.com>
2024-01-09 18:49:34 +0900

    [Silver V] Title: 영화감독 숌, Time: 196 ms, Memory: 110272 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,9 +1,11 @@
 N = int(input())
-first = 666#처음 666인 수
-while N != 0:# N 이 0이 아니면 계속 반복
-    if '666' in str(first): # 만약 666이란 문자열이 문자열(first)안에 있으면
-        N = N-1# N을 1 감소시키고
-        if N == 0:# 만약 N 이 0이면
-            break# 반복문을 탈출한다.
-    first = first + 1#first의 값을 1 증가시킨다.
-print(first)
+i = 666
+
+while True: 
+    if '666' in str(i):
+        N = N - 1
+        if N == 0:
+            break
+    i = i + 1
+    
+print(i)
